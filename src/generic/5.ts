906b659fd62850728a163e95f2a8d01230e1377b
Nikolaihait4 <jet1nikolai@gmail.com>
2023-11-18 16:58:27 +0100

    first try

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,12 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<Tkey, Tvalue> {
+  key: Tkey;
+  value: Tvalue;
 }
+
+// const pair1: KeyValuePair<string, string> = { key: "name", value: "Kristi" };
+// const pair2: KeyValuePair<number, boolean> = { key: 44, value: false };
 
 export {};
